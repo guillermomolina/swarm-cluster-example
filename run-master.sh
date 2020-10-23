54065c9faed7be9b4d9914dbbe70e40b1149f9de
root <root@instance-1.europe-west2-c.c.valid-task-293406.internal>
2020-10-23 11:36:31 +0000

    More

--- run-master.sh
+++ run-master.sh
@@ -5,8 +5,8 @@ mkdir -p /srv/nfs
 mkdir -p /opt/docker
 #	Configurar NFS
 echo 'instance-1:/srv/nfs /opt/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
-apt install -y nfs-kernel-server
 echo '/srv/nfs 10.132.0.0/24(rw,no_root_squash,no_subtree_check)' >> /etc/exports
+apt install -y nfs-kernel-server
 systemctl enable nfs-kernel-server
 systemctl start nfs-kernel-server
 mount -a
@@ -14,9 +14,6 @@ mount -a
 docker swarm init 
 docker swarm join-token manager|grep join  > /opt/docker/join.sh
 chmod +x $!
-
-cd /opt/docker
-git clone https://github.com/kpeiruza/swarm-cluster-example
 
 docker network create proxy -d overlay
 #docker network create portainer_agent -d overlay
