5e5e71157dfaad6719962e7bd14806504851cfb7
root <root@instance-1.europe-west2-c.c.valid-task-293406.internal>
2020-10-23 11:47:41 +0000

    More

--- run-master.sh
+++ run-master.sh
@@ -5,7 +5,7 @@ mkdir -p /srv/nfs
 mkdir -p /opt/docker
 #	Configurar NFS
 echo 'instance-1:/srv/nfs /opt/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
-echo '/srv/nfs 10.132.0.0/24(rw,no_root_squash,no_subtree_check)' >> /etc/exports
+echo '/srv/nfs instance-1(rw,no_root_squash,no_subtree_check) instance-2(rw,no_root_squash,no_subtree_check) instance-3(rw,no_root_squash,no_subtree_check)' >> /etc/exports
 apt install -y nfs-kernel-server
 systemctl enable nfs-kernel-server
 systemctl start nfs-kernel-server
