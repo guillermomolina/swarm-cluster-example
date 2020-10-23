30cc192782132021452d4279c05eca094db3a265
root <root@node-1.europe-west2-c.c.valid-task-293406.internal>
2020-10-23 11:07:09 +0000

    Mas cambios

--- run-deploy.sh
+++ run-deploy.sh
@@ -11,8 +11,9 @@ docker stack deploy -c traefik/docker-compose.yml traefik
 
 
 docker stack deploy -c portainer/docker-compose.yml portainer
-docker stack deploy -c consul-cluster/docker-compose.yml consul-cluster
-docker stack deploy -c comptador/docker-compose.yml comptador
+
+#docker stack deploy -c consul-cluster/docker-compose.yml consul-cluster
+#docker stack deploy -c comptador/docker-compose.yml comptador
 
 mkdir -p /opt/docker/wordpress/volumes/database
 docker stack deploy -c wordpress/docker-compose.yml wordpress
