faaa46b02ad2b2fcaf39678bbc683b4d85ed1798
Guillermo Adrian Molina <guillermoadrianmolina@hotmail.com>
2020-10-28 10:01:48 +0100

    #Fix public IP

--- run-deploy.sh
+++ run-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IP1=$(getent hosts instance-1 | awk '{print $1}')
+IP1=$(curl ifconfig.me)
 IP2=$(getent hosts instance-2 | awk '{print $1}')
 IP3=$(getent hosts instance-3 | awk '{print $1}')
 export IP1 IP2 IP3
