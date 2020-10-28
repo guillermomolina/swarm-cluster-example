ff57cb600898c6a54a5c5d27c0070b545244a4b9
Guillermo Adrian Molina <guillermoadrianmolina@hotmail.com>
2020-10-28 10:22:26 +0100

    Again

--- run-deploy.sh
+++ run-deploy.sh
@@ -9,7 +9,7 @@ export REMOTE_MOUNT="/opt/docker/data"
 BASEDIR=$(dirname $0)
 docker stack deploy -c ${BASEDIR}/traefik/docker-compose.yml traefik
 
-if [ !-d /opt/docker/portainer/volumes/portainer ]; then
+if [ ! -d /opt/docker/portainer/volumes/portainer ]; then
     mkdir -p /opt/docker/portainer/volumes/portainer
 fi
 docker stack deploy -c ${BASEDIR}/portainer/docker-compose.yml portainer
@@ -17,7 +17,7 @@ docker stack deploy -c ${BASEDIR}/portainer/docker-compose.yml portainer
 #docker stack deploy -c ${BASEDIR}/consul-cluster/docker-compose.yml consul-cluster
 #docker stack deploy -c ${BASEDIR}/comptador/docker-compose.yml comptador
 
-if [ !-d /opt/docker/wordpress/volumes/database ]; then
+if [ ! -d /opt/docker/wordpress/volumes/database ]; then
     mkdir -p /opt/docker/wordpress/volumes/database
 fi
 docker stack deploy -c ${BASEDIR}/wordpress/docker-compose.yml wordpress
