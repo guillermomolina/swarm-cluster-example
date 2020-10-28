bfc2091a4a2946f9eea8144efd98667441e0861e
Guillermo Adrian Molina <guillermoadrianmolina@hotmail.com>
2020-10-28 10:44:18 +0100

    Add elastic

--- run-deploy.sh
+++ run-deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function create_dir {
+    if [ ! -d $1 ]; then
+        mkdir -p $1
+    fi 
+}
+
+
 IP1=$(curl ifconfig.me)
 IP2=$(getent hosts instance-2 | awk '{print $1}')
 IP3=$(getent hosts instance-3 | awk '{print $1}')
@@ -9,15 +16,18 @@ export REMOTE_MOUNT="/opt/docker/data"
 BASEDIR=$(dirname $0)
 docker stack deploy -c ${BASEDIR}/traefik/docker-compose.yml traefik
 
-if [ ! -d /opt/docker/portainer/volumes/portainer ]; then
-    mkdir -p /opt/docker/portainer/volumes/portainer
-fi
+create_dir /opt/docker/portainer/volumes/portainer
 docker stack deploy -c ${BASEDIR}/portainer/docker-compose.yml portainer
 
 #docker stack deploy -c ${BASEDIR}/consul-cluster/docker-compose.yml consul-cluster
 #docker stack deploy -c ${BASEDIR}/comptador/docker-compose.yml comptador
 
-if [ ! -d /opt/docker/wordpress/volumes/database ]; then
-    mkdir -p /opt/docker/wordpress/volumes/database
-fi
+create_dir /opt/docker/wordpress/volumes/database
 docker stack deploy -c ${BASEDIR}/wordpress/docker-compose.yml wordpress
+
+create_dir /opt/docker/elastic/volumes/elasticsearch
+create_dir /opt/docker/elastic/volumes/logstash/config
+if [ ! -f /opt/docker/elastic/volumes/logstash/config/logstash.conf ]; then
+    cp ${BASEDIR}/elastic/logstash.conf /opt/docker/elastic/volumes/logstash/config
+fi
+docker stack deploy -c ${BASEDIR}/elastic/docker-compose.yml elastic
--- run-master.sh
+++ run-master.sh
@@ -5,10 +5,11 @@ mkdir -p /srv/nfs
 mkdir -p /opt/docker
 #	Configurar NFS
 echo 'instance-1:/srv/nfs /opt/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
-echo '/srv/nfs instance-1(rw,no_root_squash,no_subtree_check) instance-2(rw,no_root_squash,no_subtree_check) instance-3(rw,no_root_squash,no_subtree_check)' >> /etc/exports
 apt install -y nfs-kernel-server
 systemctl enable nfs-kernel-server
 systemctl start nfs-kernel-server
+echo '/srv/nfs instance-1(rw,no_root_squash,no_subtree_check) instance-2(rw,no_root_squash,no_subtree_check) instance-3(rw,no_root_squash,no_subtree_check)' >> /etc/exports
+systemctl restart nfs-kernel-server
 mount -a
 
 docker swarm init 
