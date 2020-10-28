5e7b23a95098efe1bd7b2067d7870ebe1722635f
Guillermo Adrian Molina <guillermoadrianmolina@hotmail.com>
2020-10-28 10:51:42 +0100

    deploy one

--- run-deploy.sh
+++ run-deploy.sh
@@ -6,28 +6,36 @@ function create_dir {
     fi 
 }
 
+function deploy {
+    docker stack deploy -c ${BASEDIR}/$1/docker-compose.yml $1
+}
 
 IP1=$(curl ifconfig.me)
 IP2=$(getent hosts instance-2 | awk '{print $1}')
 IP3=$(getent hosts instance-3 | awk '{print $1}')
 export IP1 IP2 IP3
-export REMOTE_MOUNT="/opt/docker/data"
 
 BASEDIR=$(dirname $0)
-docker stack deploy -c ${BASEDIR}/traefik/docker-compose.yml traefik
+
+if [ $# -eq 1 ]; then
+    deploy $1
+    exit 0
+fi
+
+deploy traefik
 
 create_dir /opt/docker/portainer/volumes/portainer
-docker stack deploy -c ${BASEDIR}/portainer/docker-compose.yml portainer
+deploy portainer
 
-#docker stack deploy -c ${BASEDIR}/consul-cluster/docker-compose.yml consul-cluster
-#docker stack deploy -c ${BASEDIR}/comptador/docker-compose.yml comptador
+#deploy consul-cluster
+#deploy comptador
 
 create_dir /opt/docker/wordpress/volumes/database
-docker stack deploy -c ${BASEDIR}/wordpress/docker-compose.yml wordpress
+deploy wordpress
 
 create_dir /opt/docker/elastic/volumes/elasticsearch
 create_dir /opt/docker/elastic/volumes/logstash/config
 if [ ! -f /opt/docker/elastic/volumes/logstash/config/logstash.conf ]; then
     cp ${BASEDIR}/elastic/logstash.conf /opt/docker/elastic/volumes/logstash/config
 fi
-docker stack deploy -c ${BASEDIR}/elastic/docker-compose.yml elastic
+deploy elastic
