f84477eb4d48bc7fee96ca9a6174eeaba793b034
Guillermo Adrian Molina <guillermoadrianmolina@hotmail.com>
2020-10-28 10:20:49 +0100

    Fix mkdir portainer

--- run-deploy.sh
+++ run-deploy.sh
@@ -9,11 +9,15 @@ export REMOTE_MOUNT="/opt/docker/data"
 BASEDIR=$(dirname $0)
 docker stack deploy -c ${BASEDIR}/traefik/docker-compose.yml traefik
 
-
+if [ !-d /opt/docker/portainer/volumes/portainer ]; then
+    mkdir -p /opt/docker/portainer/volumes/portainer
+fi
 docker stack deploy -c ${BASEDIR}/portainer/docker-compose.yml portainer
 
 #docker stack deploy -c ${BASEDIR}/consul-cluster/docker-compose.yml consul-cluster
 #docker stack deploy -c ${BASEDIR}/comptador/docker-compose.yml comptador
 
-mkdir -p /opt/docker/wordpress/volumes/database
+if [ !-d /opt/docker/wordpress/volumes/database ]; then
+    mkdir -p /opt/docker/wordpress/volumes/database
+fi
 docker stack deploy -c ${BASEDIR}/wordpress/docker-compose.yml wordpress
