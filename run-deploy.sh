7bcba01ee810dc9959b8c4899c8622913c74f79f
root <root@instance-1.us-central1-a.c.valid-task-293406.internal>
2020-10-28 08:29:19 +0000

    Fix cd in run-deploy.sh

--- run-deploy.sh
+++ run-deploy.sh
@@ -6,14 +6,14 @@ IP3=$(getent hosts instance-3 | awk '{print $1}')
 export IP1 IP2 IP3
 export REMOTE_MOUNT="/opt/docker/data"
 
-cd /opt/docker/swarm-cluster-example
-docker stack deploy -c traefik/docker-compose.yml traefik
+BASEDIR=$(dirname $0)
+docker stack deploy -c ${BASEDIR}/traefik/docker-compose.yml traefik
 
 
-docker stack deploy -c portainer/docker-compose.yml portainer
+docker stack deploy -c ${BASEDIR}/portainer/docker-compose.yml portainer
 
-#docker stack deploy -c consul-cluster/docker-compose.yml consul-cluster
-#docker stack deploy -c comptador/docker-compose.yml comptador
+#docker stack deploy -c ${BASEDIR}/consul-cluster/docker-compose.yml consul-cluster
+#docker stack deploy -c ${BASEDIR}/comptador/docker-compose.yml comptador
 
 mkdir -p /opt/docker/wordpress/volumes/database
-docker stack deploy -c wordpress/docker-compose.yml wordpress
+docker stack deploy -c ${BASEDIR}/wordpress/docker-compose.yml wordpress
